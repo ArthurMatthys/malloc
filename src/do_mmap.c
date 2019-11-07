1cabafea2e9b3839ddd22579957392458476b11e
Arthur Matthys <amatthys@student.42.fr>
2019-11-07 09:10:21 +0100

    Add do_mmap function

--- src/do_mmap.c
+++ src/do_mmap.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   malloc.c                                         .::    .:/ .      .::   */
+/*   do_mmap.c                                        .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: amatthys <amatthys@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/11/06 08:34:42 by amatthys     #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/07 09:07:06 by amatthys    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/11/07 09:07:15 by amatthys     #+#   ##    ##    #+#       */
+/*   Updated: 2019/11/07 09:09:22 by amatthys    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../includes/malloc.h"
 
-void		*malloc(size_t size)
+void		*do_mmap(size_t size)
 {
-	const static t_dispatcher	dispatch[] = {
-		&handle_tiny, &handle_small, &handle_large};
-	size_t						new_size;
-	int							type;
-
-	new_size = round_up(size, 16);
-	type = get_type(new_size);
-	return (dispatch[type]);
+	return (mmap(0, size, PROT_READ | PROT_WRITE,
+			MAP_ANON | MAP_PRIVATE, -1, 0));
 }
+
